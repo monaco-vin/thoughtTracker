8e982d2d5faa41580735ac0f83eea133991b96be
Rocket Appliances <monaco.vin@gmail.com>
2022-08-17 15:35:56 -0500

    ADD server.js

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./public/"));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
 /* 
 make routes file w/ apiRoutes.js and htmlRoute.js
 create html routes so that  GET /notes returns a notes.html file
